406a8ffa184ffe3af1dbfb87f649948416953280
yuhai <243746869@qq.com>
2016-12-02 08:03:08 +0800

    first commit

---
+++ Application/Admin/Controller/IndexController.class.php
@@ -0,0 +1,38 @@
+<?php
+namespace Admin\Controller;
+use Common\Controller\AdminBaseController;
+/**
+ * 后台首页控制器
+ */
+class IndexController extends AdminBaseController{
+	/**
+	 * 首页
+	 */
+	public function index(){
+		// 分配菜单数据
+		$nav_data=D('AdminNav')->getTreeData('level','order_number,id');
+		//var_dump($nav_data);exit();
+		$assign=array(
+			'data'=>$nav_data
+			);
+		$this->assign($assign);
+		$this->display();
+	}
+	/**
+	 * elements
+	 */
+	public function elements(){
+
+		$this->display();
+	}
+	
+	/**
+	 * welcome
+	 */
+	public function welcome(){
+	    $this->display();
+	}
+
+
+
+}
